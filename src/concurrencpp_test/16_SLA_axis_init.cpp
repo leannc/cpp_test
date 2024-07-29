9215ca35326042259bf1e1f410fa927e8307abfd
kang <hust.wangkang@gmail.com>
2024-07-29 09:46:16 +0800

    [V1.13.2]    1. 调整SLA的demo为task模式

--- src/concurrencpp_test/16_SLA_axis_init.cpp
+++ src/concurrencpp_test/16_SLA_axis_init.cpp
@@ -47,16 +47,15 @@ concurrencpp::generator<bool> is_moving() {
 
 result<int> axis_move_task(std::shared_ptr<concurrencpp::thread_pool_executor> tpe,std::shared_ptr<concurrencpp::timer_queue> tq,result<int>&& preivous_task,int distance,std::string axis_name)
 {
+    auto result = co_await preivous_task;
+    if (result == -1)
     {
         auto guard = co_await _lock.lock(tpe);
-        std::cout << currentTime() << axis_name << " waiting for the previous task done" <<std::this_thread::get_id()<<std::endl;
-    }
-
-    co_await preivous_task;
-
+        std::cout <<"<task begin> : "<< axis_name << " move " <<std::this_thread::get_id()<<std::endl;
+    } else
     {
         auto guard = co_await _lock.lock(tpe);
-        std::cout << currentTime() << axis_name << ": previous task done!  begin to move task " <<std::this_thread::get_id()<<std::endl;
+        std::cout<< currentTime()  << axis_name << ": previous task done!  begin to move task " <<std::this_thread::get_id()<<std::endl;
     }
 
 
@@ -85,12 +84,10 @@ result<int> axis_move_task(std::shared_ptr<concurrencpp::thread_pool_executor> t
 
 result<int> axis_go_origin_task(std::shared_ptr<concurrencpp::thread_pool_executor> tpe,std::shared_ptr<concurrencpp::timer_queue> tq,result<int>&& preivous_task,std::string axis_name)
 {
-
-
-    auto result = preivous_task.get();
+    auto result = co_await preivous_task;
     if (result == -1)
     {
-//        std::cout << axis_name << " waiting for the previous task done" <<std::this_thread::get_id()<<std::endl;
+        std::cout <<"<task begin> "<< axis_name << " go origin " <<std::this_thread::get_id()<<std::endl;
     } else
     {
         auto guard = co_await _lock.lock(tpe);
@@ -119,7 +116,7 @@ result<int> axis_go_origin_task(std::shared_ptr<concurrencpp::thread_pool_execut
             std::cout << currentTime() << axis_name <<" is still origining"<<std::endl;
         }
         //等一段时间再查询
-        co_await tq->make_delay_object(1000ms, tpe);
+        co_await tq->make_delay_object(1500ms, tpe);
     }
 
     {
@@ -171,22 +168,24 @@ int SLA_axis_init()
 
      runtime.timer_queue()->make_delay_object(0ms,thread_pool_executor);
 
+     auto recoater_platform_init_task = start_task();
+     auto resin_init_task=start_task();
     //刮刀轴回零
-    auto recoater_go_origin_result = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(start_task()),"recoater");
+    recoater_platform_init_task = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(recoater_platform_init_task),"recoater");
     //液位轴回零
-    auto resin_go_origin_result = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(start_task()),"resin");
+    resin_init_task = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(resin_init_task),"resin");
 
     //液位轴运动到指定位置
-    auto resin_go_to_some_place = axis_move_task(thread_pool_executor,runtime.timer_queue(),std::move(resin_go_origin_result),10,"resin");
+    resin_init_task = axis_move_task(thread_pool_executor,runtime.timer_queue(),std::move(resin_init_task),10,"resin");
 
     //网板回零
-    auto platform_go_origin_result = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(recoater_go_origin_result),"platform");
+    recoater_platform_init_task = axis_go_origin_task(thread_pool_executor,runtime.timer_queue(),std::move(recoater_platform_init_task),"platform");
 
     //网板运动到指定位置
-    auto platform_go_to_some_place = axis_move_task(thread_pool_executor,runtime.timer_queue(),std::move(platform_go_origin_result),10,"platform");
+    recoater_platform_init_task = axis_move_task(thread_pool_executor,runtime.timer_queue(),std::move(recoater_platform_init_task),10,"platform");
 
-    resin_go_to_some_place.get();
-    platform_go_to_some_place.get();
+    recoater_platform_init_task.get();
+    resin_init_task.get();
 
     return 0;
 }
