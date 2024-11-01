a1a1f52eb11bdd54c540493e9d57cc00a998acbf
kang <hust.wangkang@gmail.com>
2024-11-01 17:30:19 +0800

    [V1.13.15]    1. 加入visitor的默认调用方法，相当于基类实现了默认的虚函数。

--- src/design_pattern/visitor_pattern/visitor_pattern_in_std_variant_form.cpp
+++ src/design_pattern/visitor_pattern/visitor_pattern_in_std_variant_form.cpp
@@ -20,18 +20,30 @@ public:
     void DrawSquare() const {std::cout<<"square"<<std::endl;}
 };
 
+class Other {
+public:
+    void DrawOther() const {std::cout<<"other"<<std::endl;}
+};
+
+class Other2 {
+public:
+    void DrawOther() const {std::cout<<"other2"<<std::endl;}
+};
+
+
 class Draw
 {
 public:
     void operator()(Circle & c) {c.DrawCircle();}
     void operator()(Box & b) {b.DrawBox();}
     void operator()(Square & s) {s.DrawSquare();}
+    void operator()(auto & s) {s.DrawOther();}  //默认参数，如果没有匹配的，就调用这个
 };
 
-using Shape = std::variant<Circle,Box,Square>;
+using Shape = std::variant<Circle,Box,Square,Other,Other2>;
 
 void visitor_pattern_in_std_variant_form() {
-    std::vector<Shape> variant_list = {Circle{}, Box{},Square{}};
+    std::vector<Shape> variant_list = {Circle{},Other2{}, Box{},Square{},Other{}};
 
     for (auto &variant: variant_list) {
         ///这里用std::visit来写
