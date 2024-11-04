1416499c0ec154ff11a062a254e68f85aca2f237
kang <hust.wangkang@gmail.com>
2024-11-04 15:08:53 +0800

    [V1.14.2]    1. std::string_view的使用

--- src/general/std_string_view.cpp
+++ src/general/std_string_view.cpp
@@ -10,11 +10,17 @@ void print(std::string_view s)
     std::cout<<"------the string_view is : "<<s<<", the size is "<<s.size()<<".  the first address is "<<s.data()<<std::endl;
 }
 
+void modify(std::string_view s)
+{
+    s="ada";
+}
+
 
 void std_string_view()
 {
     std::string s("this is a std string");
 
+    modify(s);  //没有任何影响
     std::cout<<"string length is "<<s.size()<<", the first address is "<<s.data() <<std::endl;
     print(s);
 
