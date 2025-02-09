44c1b870aeca156e8f243d199d255cb9495eb438
kang <hust.wangkang@gmail.com>
2025-02-09 11:20:21 +0800

    抑制一些lint

--- src/benchmark/BM_with_param.cpp
+++ src/benchmark/BM_with_param.cpp
@@ -20,14 +20,14 @@ uint64_t Increment_without_optimization(uint64_t n) {
 
 
 
-void BM_with_single_param(benchmark::State& state) {
+void BM_with_single_param(benchmark::State& state) {  //NOLINT
   for (auto _ : state) {
     Increment(state.range(0));
   }
   state.SetComplexityN(state.range(0));
 }
 
-void BM_without_o2_optimization(benchmark::State& state) {
+void BM_without_o2_optimization(benchmark::State& state) {  //NOLINT
   for (auto _ : state) {
     Increment_without_optimization(state.range(0));
   }
@@ -36,7 +36,7 @@ void BM_without_o2_optimization(benchmark::State& state) {
 
 
 
-void BM_with_multy_param(benchmark::State& state) {
+void BM_with_multy_param(benchmark::State& state) {  //NOLINT
 for (auto _ : state) {
   int i1 = state.range(0);
   int i2 = state.range(1);
